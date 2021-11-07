778277a673ae9d92b2d20ab4fe419c9097513f7f
Kristina Korecova <korecovak@gmail.com>
2021-11-07 16:02:34 +0100

    Demo version

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -27,4 +27,28 @@ public class CameraController : MonoBehaviour
             transform.position += new Vector3(Mathf.Sin(Mathf.Deg2Rad * angle), 0, Mathf.Cos(Mathf.Deg2Rad * angle)) * Time.deltaTime * movSpeed;
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        /*
+         * Debug.Log(other.name);
+        if (other.gameObject.CompareTag("WallTag"))
+        {
+            Debug.Log(other.name);
+            float angle = transform.rotation.eulerAngles.y;
+            transform.position -= new Vector3(Mathf.Sin(Mathf.Deg2Rad * angle), 0, Mathf.Cos(Mathf.Deg2Rad * angle)) * Time.deltaTime * movSpeed;
+        }*/
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.name);
+        if (other.gameObject.CompareTag("WallTag"))
+        {
+            Debug.Log(other.name);
+            float angle = transform.rotation.eulerAngles.y;
+            transform.position -= new Vector3(Mathf.Sin(Mathf.Deg2Rad * angle), 0, Mathf.Cos(Mathf.Deg2Rad * angle)) * Time.deltaTime * movSpeed;
+        }
+    }
 }
